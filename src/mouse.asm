65e24f161e4b1c9540d553870f1da84428cac15c
Yokrinfh Ezpain <betochan624@gmail.com>
2026-06-23 23:23:38 -0700

    Update mouse.asm header comments and formatting

--- src/mouse.asm
+++ src/mouse.asm
@@ -1,13 +1,19 @@
-; Mouse routines for ZorroPaintASM64
-; © 2026 ZorroDev
+; ---------------------------------------------------------
+; ZorroPaintASM64 - Mouse Module
+; © 2026 ZorroDev - All Rights Reserved
+; ---------------------------------------------------------
 
 .model small
 .code
 
+;; ===========================
+;; REGION: Init Mouse
+;; ===========================
 InitMouse PROC
     mov ax, 0
     int 33h
     ret
 InitMouse ENDP
+;; END REGION
 
 end
