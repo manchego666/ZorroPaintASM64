e2af35de11d48ec0499965478fdb4127862149fb
Yokrinfh Ezpain <betochan624@gmail.com>
2026-06-23 23:55:40 -0700

    Refactor assembly code and update includes

--- src/main.asm
+++ src/main.asm
@@ -3,42 +3,51 @@
 ; © 2026 ZorroDev - All Rights Reserved
 ; ---------------------------------------------------------
 
-include ../include/constants.inc
-include ../include/macros.inc
+include include/constants.inc
+
+EXTERN InitVideo:PROC
+EXTERN InitPalette16:PROC
+EXTERN InitMouse:PROC
+EXTERN ShowMouseCursor:PROC
+
+EXTERN ShowMainMenu:PROC
+EXTERN ShowEditor:PROC
+EXTERN ExportCanvasASM:PROC
 
 .model small
 .stack 100h
 
-;; ===========================
-;; REGION: External Modules
-;; ===========================
-EXTERN InitVideo:PROC
-EXTERN InitMouse:PROC
-EXTERN ShowMainMenu:PROC
-;; END REGION
-
-
-;; ===========================
-;; REGION: Global Data
-;; ===========================
-.data
-CurrentProject db "RPG2026",0
-;; END REGION
-
-
-;; ===========================
-;; REGION: Main Code
-;; ===========================
 .code
 
 start:
+    ; segmento de datos
+    mov ax, @data
+    mov ds, ax
+
+    ; inicializar video
     call InitVideo
+
+    ; paleta profesional
+    call InitPalette16
+
+    ; mouse
     call InitMouse
+    call ShowMouseCursor
+
+MainLoop:
+    ; mostrar menú principal
     call ShowMainMenu
+
+    ; ShowMainMenu solo regresa si el usuario eligió EDITOR
+    call ShowEditor
+
+    ; después del editor, exportar 
+    ; call ExportCanvasASM
+
+    jmp MainLoop
 
 ExitProgram:
     mov ax, 4C00h
     int 21h
-;; END REGION
 
 end start
