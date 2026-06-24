f9fe908706cc1e3a6b791f0dc353e4e066c83693
Yokrinfh Ezpain <betochan624@gmail.com>
2026-06-23 23:23:06 -0700

    Refactor main.asm for better organization and clarity

--- src/main.asm
+++ src/main.asm
@@ -3,34 +3,42 @@
 ; © 2026 ZorroDev - All Rights Reserved
 ; ---------------------------------------------------------
 
+include ../include/constants.inc
+include ../include/macros.inc
+
 .model small
 .stack 100h
 
-; ---------------------------------------------------------
-; External modules (we will implement these later)
-; ---------------------------------------------------------
+;; ===========================
+;; REGION: External Modules
+;; ===========================
 EXTERN InitVideo:PROC
 EXTERN InitMouse:PROC
 EXTERN ShowMainMenu:PROC
+;; END REGION
 
+
+;; ===========================
+;; REGION: Global Data
+;; ===========================
 .data
-    ; Global variables will go here
-    CurrentProject db "RPG2026",0
+CurrentProject db "RPG2026",0
+;; END REGION
 
+
+;; ===========================
+;; REGION: Main Code
+;; ===========================
 .code
 
 start:
-    ; Initialize video mode (320x200, mode 13h)
     call InitVideo
-
-    ; Initialize mouse driver
     call InitMouse
-
-    ; Show main menu (Zorro ASCII + options)
     call ShowMainMenu
 
 ExitProgram:
     mov ax, 4C00h
     int 21h
+;; END REGION
 
 end start
