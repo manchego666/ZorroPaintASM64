5d293d23a7f56cb429ddaa7c783bb2a2aa815b9c
Yokrinfh Ezpain <betochan624@gmail.com>
2026-06-23 23:44:08 -0700

    Refactor menu drawing procedures in ui_menu.asm

--- src/ui_menu.asm
+++ src/ui_menu.asm
@@ -5,45 +5,62 @@
 
 include ../include/constants.inc
 
-EXTERN PutPixel:PROC
 EXTERN DrawRect:PROC
+EXTERN DrawString:PROC
 
 .model small
+
+.data
+MenuTitle      db "ZORROPAINT ASM64",0
+MenuOpt1       db "PROJECTS",0
+MenuOpt2       db "EDITOR",0
+MenuOpt3       db "EXIT",0
+
 .code
 
 ;; ===========================
-;; REGION: Draw Zorro ASCII (placeholder blocks)
+;; REGION: DrawMenuButton
+;; CX,DX = x,y   SI = w   DI = h
+;; DS:SI = texto (se pasa aparte)
 ;; ===========================
-DrawZorro PROC
-    mov cx, 120
-    mov dx, 40
-    mov si, 80
-    mov di, 40
-    mov al, 15
-    call DrawRect
-    ret
-DrawZorro ENDP
-;; END REGION
-
-
-;; ===========================
-;; REGION: Draw Button
-;; ===========================
-DrawButton PROC
+DrawMenuButton PROC
     push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+
     mov al, 8
     call DrawRect
+
+    add cx, 4
+    add dx, 6
+    call DrawString
+
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
     pop ax
     ret
-DrawButton ENDP
+DrawMenuButton ENDP
 ;; END REGION
 
 
 ;; ===========================
-;; REGION: Show Main Menu
+;; REGION: ShowMainMenu
 ;; ===========================
 ShowMainMenu PROC
+    push ds
+    push si
+    push cx
+    push dx
+    push si
+    push di
 
+    ; fondo
     mov cx, 0
     mov dx, 0
     mov si, SCREEN_WIDTH
@@ -51,26 +68,48 @@ ShowMainMenu PROC
     mov al, 0
     call DrawRect
 
-    call DrawZorro
+    ; título
+    mov ax, @data
+    mov ds, ax
+    mov si, OFFSET MenuTitle
+    mov cx, 80
+    mov dx, 30
+    call DrawString
 
-    mov cx, 110
-    mov dx, 120
-    mov si, 100
+    ; botón 1
+    mov si, OFFSET MenuOpt1
+    mov cx, 100
+    mov dx, 80
+    mov bx, 120
     mov di, 20
-    call DrawButton
+    mov ax, bx
+    mov si, ax
+    call DrawMenuButton
 
-    mov cx, 110
-    mov dx, 150
-    mov si, 100
+    ; botón 2
+    mov si, OFFSET MenuOpt2
+    mov cx, 100
+    mov dx, 110
+    mov ax, 120
+    mov si, ax
     mov di, 20
-    call DrawButton
+    call DrawMenuButton
 
-    mov cx, 110
-    mov dx, 180
-    mov si, 100
+    ; botón 3
+    mov si, OFFSET MenuOpt3
+    mov cx, 100
+    mov dx, 140
+    mov ax, 120
+    mov si, ax
     mov di, 20
-    call DrawButton
+    call DrawMenuButton
 
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop si
+    pop ds
     ret
 ShowMainMenu ENDP
 ;; END REGION
